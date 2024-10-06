fe88e91c6a282121f711a429d75fb0792204dec9
RominaRivarola <rivarolaromina66@gmail.com>
2024-10-05 21:54:00 -0400

    primer commit

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.primeraapp
+
+import android.os.Bundle
+import android.view.textclassifier.TextLinks.TextLink
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import com.example.primeraapp.ui.theme.PrimeraAppTheme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+             PrimeraApp()
+        }
+    }
+}
+
+@Composable
+fun Etiqueta (text: String){
+    Text(
+        text= text,
+        style= MaterialTheme.typography.headlineMedium,
+        modifier= Modifier.padding(bottom = 16. dp)
+        )
+}
+
+@Composable
+fun Entrada(value: String, onValueChange: (String) -> Unit){
+    OutlinedTextField(
+        value= value,
+        onValueChange= onValueChange,
+        label ={ Text("Ingrese un texto") },
+        modifier = Modifier.fillMaxWidth()
+
+    )
+}
+
+@Composable
+fun Boton(onClick: () -> Unit){
+    Button(
+      onClick = onClick,
+      modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("Cambiar Etiqueta")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrimeraAppPreview() {
+    PrimeraAppTheme {
+        PrimeraApp()
+    }
+}
+
+@Composable
+fun PrimeraApp(){
+    var entrada by remember { mutableStateOf(" ") }
+    var etiqueta by remember {  mutableStateOf( " ") }
+
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Etiqueta(text = etiqueta)
+                Entrada(value = entrada, onValueChange = { entrada = it})
+                Boton(onClick = { etiqueta = entrada })
+
+            }
+
+        }
+    }
+}
